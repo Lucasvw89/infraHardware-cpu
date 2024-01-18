69a72e39636769e81785d777ddab8fef3c4bf9fd
Lucas <lucasvw89@gmail.com>
2024-01-18 15:20:16 -0300

    conSrc

---
+++ componentes_feitos/mux_conSrc.v
@@ -0,0 +1,15 @@
+module mux_conSrc (
+  input wire [1:0] conSrc,
+  input wire equalTo,
+  input wire notEqualTo,
+  input wire lessThanOrEq,
+  input wire moreThan,
+  output wire [31:0] out
+);
+
+  assign out =  (conSrc == 0) ? equalTo :
+                (conSrc == 1) ? notEqualTo:
+                (conSrc == 2) ? lessThanOrEq:
+                moreThan;
+
+endmodule
