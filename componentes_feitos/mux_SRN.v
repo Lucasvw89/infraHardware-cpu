f865a75cff444fa3bacd9f6c3d16b43d91662b26
Claudino Neto <claudino.neto10@gmail.com>
2024-01-22 17:28:50 -0300

    Atualizando mux_SRN e adicionando shift_left_2

--- componentes_feitos/mux_SRN.v
+++ componentes_feitos/mux_SRN.v
@@ -1,12 +1,15 @@
-module mux_SRInput (  // incompleto
-  input wire [1:0] SrInputSrc,
+module mux_SRN (  // incompleto
+  input wire [1:0] SrNSrc,
   input wire [4:0] inFromShamt,
   input wire [31:0] inFromMem,
   input wire [4:0] inFromA,
-  input wire [4:0] inFromA,
+  input wire [4:0] inFromB,
   output wire [4:0] out,
 );
 
-  assign out = (SrInputSrc == 0) ? inFromB : inFromA;
+  assign out =  (SrNSrc == 0) ? inFromShamt:
+                (SrNSrc == 1) ? inFromMem:
+                (SrNSrc == 2) ? inFromA:
+                inFromB;
 
 endmodule
