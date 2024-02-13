5121213eef41f045cf62efa04601a6f0cb429e21
Lucas <lucasvw89@gmail.com>
2024-02-12 21:25:11 -0300

    lots of fixes to almost everything

--- componentes_feitos/load_size_control.v
+++ componentes_feitos/load_size_control.v
@@ -4,7 +4,7 @@ module load_size_control(
     output wire [23:0] out
 );
     assign out = (load_size == 2'b00) ? load_input [7:0]:
-    (load_size == 2'b01) ? load_input [15:0]:
-    (load_size == 2'b10) ? load_input [31:16]:
-    load_input [31:8];
+                 (load_size == 2'b01) ? load_input [15:0]:
+                 (load_size == 2'b10) ? load_input [31:16]:
+                 load_input [31:8];
 endmodule
